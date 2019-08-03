e9016a581d5c58224f590d48c6c6b8d4177b40f5
bartmika <bart@mikasoftware.com>
2019-08-03 17:25:18 -0400

    Added vehicle options to settings list

--- src/components/settings/settingListComponent.js
+++ src/components/settings/settingListComponent.js
@@ -74,21 +74,6 @@ class SettingListComponent extends Component {
                                     <div className="col-sm-3">
                                         <div className="card box-shadow text-center mx-auto">
                                             <div className="card-custom-top-2">
-                                                <i className="fas fa-tty fa-3x"></i>
-                                            </div>
-                                            <div className="card-body">
-                                                <h3 className="card-title">How did you hear?</h3>
-                                                <p className="card-text">Modify how users heard about you.</p>
-                                                <Link to="/settings/how-hears" className="btn btn-success btn-lg">
-                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
-                                                </Link>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-sm-3">
-                                        <div className="card box-shadow text-center mx-auto">
-                                            <div className="card-custom-top-2">
                                                 <i className="fas fa-bullhorn fa-3x"></i>
                                             </div>
                                             <div className="card-body">
@@ -100,7 +85,6 @@ class SettingListComponent extends Component {
                                             </div>
                                         </div>
                                     </div>
-
                                     <div className="col-sm-3">
                                         <div className="card box-shadow text-center mx-auto">
                                             <div className="card-custom-top-2">
@@ -115,7 +99,6 @@ class SettingListComponent extends Component {
                                             </div>
                                         </div>
                                     </div>
-
                                     <div className="col-sm-3">
                                         <div className="card box-shadow text-center mx-auto">
                                             <div className="card-custom-top-2">
@@ -130,7 +113,6 @@ class SettingListComponent extends Component {
                                             </div>
                                         </div>
                                     </div>
-
                                     <div className="col-sm-3">
                                         <div className="card box-shadow text-center mx-auto">
                                             <div className="card-custom-top-2">
@@ -145,22 +127,34 @@ class SettingListComponent extends Component {
                                             </div>
                                         </div>
                                     </div>
-
                                     <div className="col-sm-3">
                                         <div className="card box-shadow text-center mx-auto">
                                             <div className="card-custom-top-2">
-                                                <i className="fas fa-atlas fa-3x"></i>
+                                                <i className="fas fa-car fa-3x"></i>
                                             </div>
                                             <div className="card-body">
-                                                <h3 className="card-title">Resources</h3>
-                                                <p className="card-text">Add or remove resources.</p>
-                                                <Link to="/settings/resources" className="btn btn-success btn-lg">
+                                                <h3 className="card-title">Vehicle Type</h3>
+                                                <p className="card-text">Modify vehicle types for associate.</p>
+                                                <Link to="/settings/vehicle-types" className="btn btn-success btn-lg">
                                                     Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
                                                 </Link>
                                             </div>
                                         </div>
                                     </div>
-
+                                    <div className="col-sm-3">
+                                        <div className="card box-shadow text-center mx-auto">
+                                            <div className="card-custom-top-2">
+                                                <i className="fas fa-tty fa-3x"></i>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title">How did you hear?</h3>
+                                                <p className="card-text">Modify how users heard about you.</p>
+                                                <Link to="/settings/how-hears" className="btn btn-success btn-lg">
+                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
 
                             </div>
