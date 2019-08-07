6bec3a4ca901752c62a474c8e95bdd8a343b558d
bartmika <bart@mikasoftware.com>
2019-08-07 10:42:59 -0400

    Expandend staff APIs

--- src/actions/staffActions.js
+++ src/actions/staffActions.js
@@ -1,44 +1,21 @@
 import axios from 'axios';
 import store from '../store';
-import { camelizeKeys, decamelize } from 'humps';
+import { camelizeKeys, decamelize, decamelizeKeys } from 'humps';
 import isEmpty from 'lodash/isEmpty';
 import msgpack from 'msgpack-lite';
 
 import {
-    STAFF_LIST_REQUEST,
-    STAFF_LIST_FAILURE,
-    STAFF_LIST_SUCCESS
+    STAFF_LIST_REQUEST, STAFF_LIST_FAILURE, STAFF_LIST_SUCCESS,
+    STAFF_DETAIL_REQUEST, STAFF_DETAIL_FAILURE, STAFF_DETAIL_SUCCESS
 } from '../constants/actionTypes';
-import { WORKERY_STAFF_LIST_API_ENDPOINT } from '../constants/api';
+import { WORKERY_STAFF_LIST_API_ENDPOINT, WORKERY_STAFF_DETAIL_API_ENDPOINT } from '../constants/api';
 import getCustomAxios from '../helpers/customAxios';
 
 
-export const setStaffListRequest = () => ({
-    type: STAFF_LIST_REQUEST,
-    payload: {
-        isAPIRequestRunning: true,
-        page: 1,
-        errors: {}
-    },
-});
+////////////////////////////////////////////////////////////////////////////////
+//                                 LIST                                       //
+////////////////////////////////////////////////////////////////////////////////
 
-
-export const setStaffListFailure = (info) => ({
-    type: STAFF_LIST_FAILURE,
-    payload: info,
-});
-
-
-export const setStaffListSuccess = (info) => ({
-    type: STAFF_LIST_SUCCESS,
-    payload: info,
-});
-
-
-/**
- *  Function will pull the ``instrument`` API endpoint and override our
- *  global application state for the 'dashboard'.
- */
 export function pullStaffList(page=1, sizePerPage=10, filtersMap=new Map(), onSuccessCallback=null, onFailureCallback=null) {
     return dispatch => {
         // Change the global state to attempting to fetch latest user details.
@@ -123,3 +100,266 @@ export function pullStaffList(page=1, sizePerPage=10, filtersMap=new Map(), onSu
 
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//                                 CREATE                                     //
+////////////////////////////////////////////////////////////////////////////////
+
+export function postStaffDetail(postData, successCallback, failedCallback) {
+    return dispatch => {
+        // Change the global state to attempting to log in.
+        store.dispatch(
+            setStaffDetailRequest()
+        );
+
+        // Generate our app's Axios instance.
+        const customAxios = getCustomAxios();
+
+        // The following code will convert the `camelized` data into `snake case`
+        // data so our API endpoint will be able to read it.
+        let decamelizedData = decamelizeKeys(postData);
+
+        // Encode from JS Object to MessagePack (Buffer)
+        var buffer = msgpack.encode(decamelizedData);
+
+        // Perform our API submission.
+        customAxios.post(WORKERY_STAFF_LIST_API_ENDPOINT, buffer).then( (successResponse) => {
+            // Decode our MessagePack (Buffer) into JS Object.
+            const responseData = msgpack.decode(Buffer(successResponse.data));
+
+            let device = camelizeKeys(responseData);
+
+            // Extra.
+            device['isAPIRequestRunning'] = false;
+            device['errors'] = {};
+
+            // Run our success callback function.
+            successCallback(device);
+
+            // Update the global state of the application to store our
+            // user device for the application.
+            store.dispatch(
+                setStaffDetailSuccess(device)
+            );
+        }).catch( (exception) => {
+            if (exception.response) {
+                const responseBinaryData = exception.response.data; // <=--- NOTE: https://github.com/axios/axios/issues/960
+
+                // Decode our MessagePack (Buffer) into JS Object.
+                const responseData = msgpack.decode(Buffer(responseBinaryData));
+
+                let errors = camelizeKeys(responseData);
+
+                console.log("postStaffDetail | error:", errors); // For debuggin purposes only.
+
+                // Send our failure to the redux.
+                store.dispatch(
+                    setStaffDetailFailure({
+                        isAPIRequestRunning: false,
+                        errors: errors
+                    })
+                );
+
+                // DEVELOPERS NOTE:
+                // IF A CALLBACK FUNCTION WAS SET THEN WE WILL RETURN THE JSON
+                // OBJECT WE GOT FROM THE API.
+                if (failedCallback) {
+                    failedCallback(errors);
+                }
+            }
+
+        }).then( () => {
+            // Do nothing.
+        });
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//                                RETRIEVE                                    //
+////////////////////////////////////////////////////////////////////////////////
+
+export function pullStaffDetail(user, slug) {
+    return dispatch => {
+        // Change the global state to attempting to fetch latest user details.
+        store.dispatch(
+            setStaffDetailRequest()
+        );
+
+        // Generate our app's Axios instance.
+        const customAxios = getCustomAxios();
+
+        const aURL = WORKERY_STAFF_DETAIL_API_ENDPOINT+slug;
+
+        customAxios.get(aURL).then( (successResponse) => { // SUCCESS
+            // Decode our MessagePack (Buffer) into JS Object.
+            const responseData = msgpack.decode(Buffer(successResponse.data));
+            // console.log(successResult); // For debugging purposes.
+
+            let profile = camelizeKeys(responseData);
+
+            // Extra.
+            profile['isAPIRequestRunning'] = false;
+            profile['errors'] = {};
+
+            console.log("pullStaffDetail | Success:", profile); // For debugging purposes.
+
+            // Update the global state of the application to store our
+            // user profile for the application.
+            store.dispatch(
+                setStaffDetailSuccess(profile)
+            );
+
+        }).catch( (exception) => { // ERROR
+            if (exception.response) {
+                const responseBinaryData = exception.response.data; // <=--- NOTE: https://github.com/axios/axios/issues/960
+
+                // Decode our MessagePack (Buffer) into JS Object.
+                const responseData = msgpack.decode(Buffer(responseBinaryData));
+
+                let errors = camelizeKeys(responseData);
+
+                console.log("pullStaffDetail | error:", errors); // For debuggin purposes only.
+
+                // Send our failure to the redux.
+                store.dispatch(
+                    setStaffDetailFailure({
+                        isAPIRequestRunning: false,
+                        errors: errors
+                    })
+                );
+
+                // // DEVELOPERS NOTE:
+                // // IF A CALLBACK FUNCTION WAS SET THEN WE WILL RETURN THE JSON
+                // // OBJECT WE GOT FROM THE API.
+                // if (failedCallback) {
+                //     failedCallback(errors);
+                // }
+            }
+
+        }).then( () => { // FINALLY
+            // Do nothing.
+        });
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//                                UPDATE                                      //
+////////////////////////////////////////////////////////////////////////////////
+
+export function putStaffDetail(user, data, successCallback, failedCallback) {
+    return dispatch => {
+        // Change the global state to attempting to log in.
+        store.dispatch(
+            setStaffDetailRequest()
+        );
+
+        // Generate our app's Axios instance.
+        const customAxios = getCustomAxios();
+
+        // The following code will convert the `camelized` data into `snake case`
+        // data so our API endpoint will be able to read it.
+        let decamelizedData = decamelizeKeys(data);
+
+        // Encode from JS Object to MessagePack (Buffer)
+        var buffer = msgpack.encode(decamelizedData);
+
+        // Perform our API submission.
+        customAxios.put(WORKERY_STAFF_DETAIL_API_ENDPOINT+data.slug, buffer).then( (successResponse) => {
+            // Decode our MessagePack (Buffer) into JS Object.
+            const responseData = msgpack.decode(Buffer(successResponse.data));
+            let device = camelizeKeys(responseData);
+
+            // Extra.
+            device['isAPIRequestRunning'] = false;
+            device['errors'] = {};
+
+            // Update the global state of the application to store our
+            // user device for the application.
+            store.dispatch(
+                setStaffDetailSuccess(device)
+            );
+
+            // Run our success callback function.
+            successCallback(device);
+
+        }).catch( (exception) => {
+            if (exception.response) {
+                const responseBinaryData = exception.response.data; // <=--- NOTE: https://github.com/axios/axios/issues/960
+
+                // Decode our MessagePack (Buffer) into JS Object.
+                const responseData = msgpack.decode(Buffer(responseBinaryData));
+
+                let errors = camelizeKeys(responseData);
+
+                console.log("putStaffDetail | error:", errors); // For debuggin purposes only.
+
+                // Send our failure to the redux.
+                store.dispatch(
+                    setStaffDetailFailure({
+                        isAPIRequestRunning: false,
+                        errors: errors
+                    })
+                );
+
+                // DEVELOPERS NOTE:
+                // IF A CALLBACK FUNCTION WAS SET THEN WE WILL RETURN THE JSON
+                // OBJECT WE GOT FROM THE API.
+                if (failedCallback) {
+                    failedCallback(errors);
+                }
+            }
+
+        }).then( () => {
+            // Do nothing.
+        });
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//                                REDUX ACTIONS                               //
+////////////////////////////////////////////////////////////////////////////////
+
+export const setStaffListRequest = () => ({
+    type: STAFF_LIST_REQUEST,
+    payload: {
+        isAPIRequestRunning: true,
+        page: 1,
+        errors: {}
+    },
+});
+
+
+export const setStaffListFailure = (info) => ({
+    type: STAFF_LIST_FAILURE,
+    payload: info,
+});
+
+
+export const setStaffListSuccess = (info) => ({
+    type: STAFF_LIST_SUCCESS,
+    payload: info,
+});
+
+
+export const setStaffDetailRequest = () => ({
+    type: STAFF_DETAIL_REQUEST,
+    payload: {
+        isAPIRequestRunning: true,
+        errors: {}
+    },
+});
+
+
+export const setStaffDetailSuccess = staffDetail => ({
+    type: STAFF_DETAIL_SUCCESS,
+    payload: staffDetail,
+});
+
+
+export const setStaffDetailFailure = staffDetail => ({
+    type: STAFF_DETAIL_FAILURE,
+    payload: staffDetail,
+});
