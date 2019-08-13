72669e420041ebf4c5933198f8e3851e5a0e3b26
bartmika <bart@mikasoftware.com>
2019-08-13 13:55:19 -0400

    Added tables

--- src/components/orders/retrieve/orderFullRetrieveComponent.js
+++ src/components/orders/retrieve/orderFullRetrieveComponent.js
@@ -264,6 +264,35 @@ export default class OrderFullRetrieveComponent extends Component {
                                     </td>
                                 </tr>
 
+
+                                <tr className="bg-dark">
+                                    <th scope="row" colSpan="2" className="text-light">
+                                        <i className="fas fa-project-diagram"></i>&nbsp;Functions
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Available Choices</th>
+                                    <td>
+                                        <ul>
+                                            <li>
+                                                <Link to={`#`}>
+                                                    Transfer&nbsp;<i className="fas fa-chevron-right"></i>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to={`#`}>
+                                                    Postpone&nbsp;<i className="fas fa-chevron-right"></i>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to={`#`}>
+                                                    Close&nbsp;<i className="fas fa-chevron-right"></i>
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </td>
+                                </tr>
+
                             </tbody>
                         </table>
                     </div>
