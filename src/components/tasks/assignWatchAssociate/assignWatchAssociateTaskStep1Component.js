8c41f849ff13f334695ae3c3c7aae908ed3e5310
bartmika <bart@mikasoftware.com>
2019-08-23 15:24:51 -0400

    Latest progress with fetching the task retrieve page.

--- src/components/tasks/assignWatchAssociate/assignWatchAssociateTaskStep1Component.js
+++ src/components/tasks/assignWatchAssociate/assignWatchAssociateTaskStep1Component.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-import { FlashMessageComponent } from "../../flashMessageComponent";
-
 
 class AssignWatchAssociateTaskStep1Component extends Component {
     render() {
-        const { urlArgument, onClick, onBack, flashMessage } = this.props;
+        const { id, task, onBack, onClick } = this.props;
         return (
             <div>
                 <nav aria-label="breadcrumb">
@@ -22,8 +20,6 @@ class AssignWatchAssociateTaskStep1Component extends Component {
                         </li>
                     </ol>
                 </nav>
-
-                <FlashMessageComponent object={flashMessage} />
 
                 <h1><i className="fas fa-thumbtack"></i>&nbsp;Argyle</h1>
 
@@ -55,25 +51,6 @@ class AssignWatchAssociateTaskStep1Component extends Component {
                             <tr>
                                 <th scope="row" className="bg-light">Description</th>
                                 <td>A new watch has been created and requires an associate to be assigned. Please assign an associate to the watch.</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" className="bg-light">Watch</th>
-                                <td>Argyle Community Watch</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" className="bg-light">Tags</th>
-                                <td><span className="badge badge-info badge-lg">Skill 1</span> <span
-                                        className="badge badge-info badge-lg">Skill 2</span>
-                                    <span className="badge badge-info badge-lg">HTML</span> <span
-                                            className="badge badge-info badge-lg">PHP</span> <span
-                                            className="badge badge-info badge-lg">Bootstrap</span> <span
-                                            className="badge badge-info badge-lg">Django</span> <span
-                                            className="badge badge-info badge-lg">Photoshop</span> <span
-                                            className="badge badge-info badge-lg">Python</span> <span
-                                            className="badge badge-info badge-lg">ASP.net</span> <span
-                                            className="badge badge-info badge-lg">Android</span> <span
-                                            className="badge badge-info badge-lg">jQuery</span>
-                                </td>
                             </tr>
                             </tbody>
                         </table>
