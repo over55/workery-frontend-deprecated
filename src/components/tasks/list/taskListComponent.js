03670c3f01c6d1bf8b87ea450242c5d4a0f0ff0a
bartmika <bart@mikasoftware.com>
2019-08-31 12:43:16 -0400

    BUGFIX: Added better handling to the task list.

--- src/components/tasks/list/taskListComponent.js
+++ src/components/tasks/list/taskListComponent.js
@@ -211,8 +211,13 @@ function financialExternalLinkFormatter(cell, row){
 
 function detailLinkFormatter(cell, row){
     if (row.typeOf === 3) { // Add code to prevent the deprecated task from being accessible.
-        return "[Disabled]"
+        return (
+            <div>
+                <i className="fas fa-lock"></i>&nbsp;Locked
+            </div>
+        );
     }
+    if (row.isClosed === true) { return "Closed"; }
     return (
         <Link to={`/task/${row.typeOf}/${row.id}/step-1`}>
             View&nbsp;<i className="fas fa-chevron-right"></i>
