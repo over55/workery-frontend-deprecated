2e1f50b3b0449f14a5bdc64d74423ba7be874299
bartmika <bart@mikasoftware.com>
2019-09-22 18:42:49 -0400

    BUGFIX: Fixed assigned icon in partner. #37

--- src/components/partners/retrieve/file_upload/partnerFileUploadArchiveComponent.js
+++ src/components/partners/retrieve/file_upload/partnerFileUploadArchiveComponent.js
@@ -29,7 +29,7 @@ export default class PartnerFileUploadArchiveComponent extends Component {
                            <Link to="/dashboard"><i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard</Link>
                         </li>
                         <li className="breadcrumb-item" aria-current="page">
-                            <Link to="/partners"><i className="fas fa-user-circle"></i>&nbsp;Partners</Link>
+                            <Link to="/partners"><i className="fas fa-handshake"></i>&nbsp;Partners</Link>
                         </li>
                         <li className="breadcrumb-item active" aria-current="page">
                             <i className="fas fa-user"></i>&nbsp;{partner && partner.fullName}
