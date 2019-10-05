0c9cadc8afbf6c30bf1fd47147c2fec71d09370a
bartmika <bart@mikasoftware.com>
2019-10-05 19:26:38 -0400

    Relabelled button. #152

--- src/components/tasks/orderCompletion/orderCompletionTaskStep4Component.js
+++ src/components/tasks/orderCompletion/orderCompletionTaskStep4Component.js
@@ -107,7 +107,7 @@ export default class OrderCompletionTaskStep4Component extends Component {
 
                         <div className="form-group col-md-12 mb-3 p-0 mx-auto text-center">
                             <button className="btn btn-primary btn-lg mt-4 float-right pl-4 pr-4" onClick={onClick} isLoading={isLoading}>
-                                Proceed to Review&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                Review&nbsp;<i className="fas fa-arrow-circle-right"></i>
                             </button>
 
                             <Link className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4" to={`/task/6/${id}/step-3`}>
