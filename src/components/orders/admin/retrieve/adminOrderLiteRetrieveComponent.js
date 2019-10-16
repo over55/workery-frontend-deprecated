2746c68826cc28a68f685e6c2c03fce53e9d4faa
bartmika <bart@mikasoftware.com>
2019-10-16 12:24:21 -0400

    BUGFIX: Fixed the URL for order transfers.

--- src/components/orders/admin/retrieve/adminOrderLiteRetrieveComponent.js
+++ src/components/orders/admin/retrieve/adminOrderLiteRetrieveComponent.js
@@ -211,7 +211,7 @@ export default class AdminOrderLiteRetrieveComponent extends Component {
                                             {isCancelled
                                                 ?""
                                                 :<li>
-                                                    <Link to={`/order/${order.id}/transfer`}>
+                                                    <Link to={`/order/${order.id}/transfer-step-1`}>
                                                         Transfer&nbsp;<i className="fas fa-chevron-right"></i>
                                                     </Link>
                                                 </li>
