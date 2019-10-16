88d8f6f4ec47f5cef4498edc65e23bc0269f4f45
bartmika <bart@mikasoftware.com>
2019-10-15 23:32:27 -0400

    Added retrieve handling. #185

--- src/containers/financials/retrieve/financialRetrieveContainer.js
+++ src/containers/financials/retrieve/financialRetrieveContainer.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
 
 import FinancialRetrieveComponent from "../../../components/financials/retrieve/financialRetrieveComponent";
 import { pullOrderDetail } from "../../../actions/orderActions";
 import { clearFlashMessage } from "../../../actions/flashMessageActions";
+import {
+    localStorageGetObjectItem, localStorageSetObjectOrArrayItem
+} from '../../../helpers/localStorageUtility';
 
 
 class FinancialRetrieveContainer extends Component {
@@ -17,11 +21,16 @@ class FinancialRetrieveContainer extends Component {
 
         const { id } = this.props.match.params;
 
+        // The following code will extract our financial data from the local
+        // storage if the financial data was previously saved.
+        const financial = localStorageGetObjectItem("workery-retrieve-financial-"+id.toString() );
+        const isLoading = isEmpty(financial);
+
         // Update state.
         this.state = {
             id: parseInt(id),
-            isLoading: true,
-            order: {}
+            isLoading: isLoading,
+            financial: financial
         }
 
         // Update functions.
@@ -58,7 +67,11 @@ class FinancialRetrieveContainer extends Component {
 
     onSuccessCallback(response) {
         console.log(response);
-        this.setState({ isLoading: false, });
+        this.setState({ isLoading: false, financial: response, });
+
+        // The following code will save the object to the browser's local
+        // storage to be retrieved later more quickly.
+        localStorageSetObjectOrArrayItem("workery-retrieve-financial-"+this.state.id.toString(), response);
     }
 
     onFailureCallback(errors) {
@@ -77,12 +90,12 @@ class FinancialRetrieveContainer extends Component {
      */
 
     render() {
-        const order = this.props.orderDetail ? this.props.orderDetail : {};
+        const financial = this.state.financial ? this.state.financial : {};
         return (
             <FinancialRetrieveComponent
                 id={this.state.id}
                 isLoading={this.state.isLoading}
-                order={order}
+                order={financial}
                 flashMessage={this.props.flashMessage}
             />
         );
