34b01b8e7f0b38ca717a3b7cb09b231025911a09
bartmika <bart@mikasoftware.com>
2019-10-25 15:28:18 -0400

    More changes. #252

--- src/components/tasks/orderCompletion/orderCompletionTaskStep2Component.js
+++ src/components/tasks/orderCompletion/orderCompletionTaskStep2Component.js
@@ -170,11 +170,11 @@ export default class OrderCompletionTaskStep2Component extends Component {
                         </form>
 
                         <div className="form-group col-md-12 mb-3 p-0 mx-auto text-center">
-                            <button className="btn btn-primary btn-lg mt-4 float-right pl-4 pr-4" onClick={onClick} isLoading={isLoading}>
-                                Financials&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                            <button className="btn btn-success btn-lg mt-4 float-right pl-4 pr-4" onClick={onClick} isLoading={isLoading}>
+                                Next&nbsp;<i className="fas fa-arrow-circle-right"></i>
                             </button>
 
-                            <Link className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4" to={`/task/6/${id}/step-1`}>
+                            <Link className="btn btn-orange btn-lg mt-4 float-left pl-4 pr-4" to={`/task/6/${id}/step-1`}>
                                 <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
                             </Link>
                         </div>
