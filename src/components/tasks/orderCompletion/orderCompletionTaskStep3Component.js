3fe6650a6908a484ab9df0e855e4f1b607eda6c9
bartmika <bart@mikasoftware.com>
2019-10-25 15:32:10 -0400

    More text changes. #253

--- src/components/tasks/orderCompletion/orderCompletionTaskStep3Component.js
+++ src/components/tasks/orderCompletion/orderCompletionTaskStep3Component.js
@@ -419,11 +419,11 @@ export default class OrderCompletionTaskStep3Component extends Component {
                         </form>
 
                         <div className="form-group col-md-12 mb-3 p-0 mx-auto text-center">
-                            <button className="btn btn-primary btn-lg mt-4 float-right pl-4 pr-4" onClick={onClick} isLoading={isLoading}>
-                                Comment&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                            <button className="btn btn-success btn-lg mt-4 float-right pl-4 pr-4" onClick={onClick} isLoading={isLoading}>
+                                Next&nbsp;<i className="fas fa-arrow-circle-right"></i>
                             </button>
 
-                            <Link className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4" to={`/task/6/${id}/step-2`}>
+                            <Link className="btn btn-orange btn-lg mt-4 float-left pl-4 pr-4" to={`/task/6/${id}/step-2`}>
                                 <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
                             </Link>
                         </div>
