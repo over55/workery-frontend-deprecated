2a7cef272360a77828051716ed2554a594ab6369
bartmika <bart@mikasoftware.com>
2019-10-25 14:01:58 -0400

    Implemented changes to the task page. #242

--- src/components/tasks/assignAssociate/assignAssociateTaskStep1Component.js
+++ src/components/tasks/assignAssociate/assignAssociateTaskStep1Component.js
@@ -125,21 +125,46 @@ export default class AssignAssociateTaskStep1Component extends Component {
                                         </Link>
                                     </td>
                                 </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Created at:</th>
+                                    <td>
+                                        {task && task.createdAt &&
+                                            <Moment format="MM/DD/YYYY">{task.createdAt}</Moment>
+                                        }
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Created by:</th>
+                                    <td>{task && task.createdByLabel}
+                                    </td>
+                                </tr>
 
                                 <tr className="bg-dark">
                                     <th scope="row" colSpan="2" className="text-light">
-                                        <i className="fas fa-project-diagram"></i>&nbsp;Functions
+                                        <i className="fas fa-clipboard-list"></i>&nbsp;Requied Tasks
                                     </th>
                                 </tr>
                                 <tr>
                                     <th scope="row" className="bg-light">Available Choices</th>
                                     <td>
-                                        <Link className="btn btn-orange btn-lg btn-fxw mr-3" to={`/order/${task.job}/postpone`}>
-                                            <i className="fas fa-clock"></i>&nbsp;Postpone Order
-                                        </Link>
-                                        <Link className="btn btn-danger btn-lg btn-fxw" to={`/order/${task.job}/close`}>
-                                            <i className="fas fa-window-close"></i>&nbsp;Cancel Job
-                                        </Link>
+                                        <div className="row">
+                                            <div className="col-md-4 mx-auto p-2">
+                                                <Link className="btn btn-orange btn-lg" to={`/order/${task.job}/postpone`}>
+                                                    <i className="fas fa-clock"></i>&nbsp;Postpone Order
+                                                </Link>
+                                            </div>
+                                            <div className="col-md-4 mx-auto p-2">
+                                                <Link className="btn btn-danger btn-lg" to={`/order/${task.job}/close`}>
+                                                    <i className="fas fa-window-close"></i>&nbsp;Cancel Job
+                                                </Link>
+                                            </div>
+                                            <div className="col-md-4 mx-auto p-2">
+                                                <button className="btn btn-success btn-lg" onClick={onClick}>
+                                                    Begin&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+
                                     </td>
                                 </tr>
 
@@ -147,9 +172,7 @@ export default class AssignAssociateTaskStep1Component extends Component {
                         </table>
 
                         <div className="form-group col-md-12 mb-3 p-0 mx-auto text-center">
-                            <button className="btn btn-primary btn-lg mt-4 float-right pl-4 pr-4" onClick={onClick}>
-                                Selection&nbsp;<i className="fas fa-arrow-circle-right"></i>
-                            </button>
+
 
                             <Link className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4" to="/tasks">
                                 <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
@@ -158,8 +181,6 @@ export default class AssignAssociateTaskStep1Component extends Component {
 
                     </div>
                 </div>
-
-
             </div>
         );
     }
