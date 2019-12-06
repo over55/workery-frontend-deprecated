1c66a9ccd99e6951540f4a19512333478821755e
bartmika <bart@mikasoftware.com>
2019-12-05 21:46:39 -0500

    TYPOFIX: #340

--- src/components/clients/create/clientCreateStep4Component.js
+++ src/components/clients/create/clientCreateStep4Component.js
@@ -216,8 +216,8 @@ class ClientCreateStep4Component extends Component {
 
 
                             <div className="form-group">
-                                <button className="btn btn-primary btn-lg mt-4 float-right pl-4 pr-4" disabled={isLoading} onClick={onClick}>
-                                    Address&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                <button className="btn btn-success btn-lg mt-4 float-right pl-4 pr-4" disabled={isLoading} onClick={onClick}>
+                                    Next&nbsp;<i className="fas fa-arrow-circle-right"></i>
                                 </button>
                                 <Link to="/clients/add/step-3" className="btn btn-orange btn-lg mt-4 float-left pl-4 pr-4">
                                     <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
