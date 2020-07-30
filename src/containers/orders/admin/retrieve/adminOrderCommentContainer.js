ea9b2bfd17ceb086a7773b07be3583901f51940d
bartmika <bart@mikasoftware.com>
2020-07-29 22:49:08 -0400

    BUGFIX: Clear the text when making a comment submission. https://github.com/over55/workery-front/issues/388

--- src/containers/orders/admin/retrieve/adminOrderCommentContainer.js
+++ src/containers/orders/admin/retrieve/adminOrderCommentContainer.js
@@ -105,6 +105,7 @@ class OrderCommentContainer extends Component {
                 page: response.page,
                 totalSize: response.count,
                 isLoading: false,
+                text: "",
             },
             ()=>{
                 console.log("onSuccessListCallback | Fetched:",response); // For debugging purposes only.
@@ -115,7 +116,7 @@ class OrderCommentContainer extends Component {
 
     onFailureListCallback(errors) {
         console.log(errors);
-        this.setState({ isLoading: false });
+        this.setState({ isLoading: false, text: "", });
     }
 
     onSuccessPostCallback(response) {
@@ -125,6 +126,7 @@ class OrderCommentContainer extends Component {
                 page: response.page,
                 totalSize: response.count,
                 isLoading: false,
+                text: "",
             },
             ()=>{
                 console.log("onSuccessPostCallback | Fetched:",response); // For debugging purposes only.
