8b096fb3ead78cc76bf7642479a4d0a873c4bef9
bartmika <bart@mikasoftware.com>
2021-07-24 17:40:31 -0400

    (1) Added `env-cmd` package. (2) BUGFIX: Fixed field in login page.

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -50,12 +50,12 @@ export function postLogin(email, password, successCallback=null, failedCallback=
 
         // Encode from JS Object to MessagePack (Buffer)
         var buffer = msgpack.encode({
-            'email_or_username': email,
+            'email': email,
             'password': password,
         });
 
         var data = {
-            'email_or_username': email,
+            'email': email,
             'password': password,
         };
 
