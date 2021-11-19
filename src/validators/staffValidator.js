15d904f46f5944337aa88aed9f3ed29ae7ba339b
bartmika <bart@mikasoftware.com>
2021-11-19 00:56:19 -0500

    Impl. contact update.

--- src/validators/staffValidator.js
+++ src/validators/staffValidator.js
@@ -8,6 +8,111 @@ import {
 } from '../constants/api';
 
 
+export function validateContactInput(data) {
+    let errors = {};
+
+    // --- CONTACT --- //
+
+    if (data.typeOf === RESIDENTIAL_CUSTOMER_TYPE_OF_ID) {
+
+        if (data.givenName === undefined || data.givenName === null || validator.isEmpty(data.givenName) || data.givenName === "") {
+            errors.givenName = 'This field is required';
+        }
+        if (data.lastName === undefined || data.lastName === null || validator.isEmpty(data.lastName) || data.lastName === "") {
+            errors.lastName = 'This field is required';
+        }
+        if (data.telephone === undefined || data.telephone === null || validator.isEmpty(data.telephone) || data.telephone === "") {
+            errors.telephone = 'This field is required';
+        }
+        // if (data.otherTelephone === undefined || data.otherTelephone === null || validator.isEmpty(data.otherTelephone) || data.otherTelephone === "") {
+        //     errors.otherTelephone = 'This field is required';
+        // }
+        if (data.isOkToEmail === undefined || data.isOkToEmail === null || data.isOkToEmail === "" || isNaN(data.isOkToEmail)) {
+            errors.isOkToEmail = 'This field is required';
+        }
+        if (data.isOkToText === undefined || data.isOkToText === null || data.isOkToText === "" || isNaN(data.isOkToText) ) {
+            errors.isOkToText = 'This field is required';
+        }
+
+    } else if (data.typeOf === COMMERCIAL_CUSTOMER_TYPE_OF_ID) {
+
+        if (data.organizationName === undefined || data.organizationName === null || validator.isEmpty(data.organizationName) || data.organizationName === "") {
+            errors.organizationName = 'This field is required';
+        }
+        if (data.organizationTypeOf === undefined || data.organizationTypeOf === null || data.organizationTypeOf === "") {
+            errors.organizationTypeOf = 'This field is required';
+        }
+        if (data.givenName === undefined || data.givenName === null || validator.isEmpty(data.givenName) || data.givenName === "") {
+            errors.givenName = 'This field is required';
+        }
+        if (data.lastName === undefined || data.lastName === null || validator.isEmpty(data.lastName) || data.lastName === "") {
+            errors.lastName = 'This field is required';
+        }
+        if (data.telephone === undefined || data.telephone === null || validator.isEmpty(data.telephone) || data.telephone === "") {
+            errors.telephone = 'This field is required';
+        }
+        // if (data.otherTelephone === undefined || data.otherTelephone === null || validator.isEmpty(data.otherTelephone) || data.otherTelephone === "") {
+        //     errors.otherTelephone = 'This field is required';
+        // }
+    }
+
+    // For debugging purposes only.
+    console.log("validateContactInput | post-validation", data);
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+
+export function validateAddressInput(data) {
+    let errors = {};
+
+    if (data.streetAddress === undefined || data.streetAddress === null || validator.isEmpty(data.streetAddress) || data.streetAddress === "") {
+        errors.streetAddress = 'This field is required';
+    }
+    if (data.region === undefined || data.region === null || validator.isEmpty(data.region) || data.region === "") {
+        errors.region = 'This field is required';
+    }
+    if (data.locality === undefined || data.locality === null || validator.isEmpty(data.locality) || data.locality === "") {
+        errors.locality = 'This field is required';
+    }
+    if (data.country === undefined || data.country === null || validator.isEmpty(data.country) || data.country === "") {
+        errors.country = 'This field is required';
+    }
+    if (data.postalCode === undefined || data.postalCode === null || validator.isEmpty(data.postalCode) || data.postalCode === "") {
+        errors.postalCode = 'This field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+
+
+export function validateMetricsInput(data) {
+    let errors = {};
+
+    if (data.gender === undefined || data.gender === null || data.gender === "") {
+        errors.gender = 'This field is required';
+    }
+    if (data.howHearId === undefined || data.howHearId === null || data.howHearId === "" || isNaN(data.howHearId) ) {
+        errors.howHearId = 'This field is required';
+    }
+    if (data.joinDate === undefined || data.joinDate === null || data.joinDate === "" || isNaN(data.joinDate) ) {
+        errors.joinDate = 'This field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+
 export function validateInput(data) {
     let errors = {};
 
