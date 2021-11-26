23f4cc8b824e5c7fac28ba0822a0c153ecaae33d
bartmika <bart@mikasoftware.com>
2021-11-26 00:24:37 -0500

    Insurance requirements CRUD works.

--- src/actions/insuranceRequirementActions.js
+++ src/actions/insuranceRequirementActions.js
@@ -177,7 +177,7 @@ export function pullInsuranceRequirementDetail(id, onSuccessCallback, onFailureC
         // Generate our app's Axios instance.
         const customAxios = getCustomAxios();
 
-        const aURL = WORKERY_INSURANCE_REQUIREMENT_DETAIL_API_ENDPOINT+id+"/";
+        const aURL = WORKERY_INSURANCE_REQUIREMENT_DETAIL_API_ENDPOINT+id;
 
         customAxios.get(aURL).then( (successResponse) => { // SUCCESS
             const responseData = successResponse.data;
@@ -254,7 +254,7 @@ export function putInsuranceRequirementDetail(postData, successCallback, failedC
         let decamelizedData = decamelizeKeys(postData);
 
         // Perform our API submission.
-        customAxios.put(WORKERY_INSURANCE_REQUIREMENT_DETAIL_API_ENDPOINT+postData.id+"/", decamelizedData).then( (successResponse) => {
+        customAxios.put(WORKERY_INSURANCE_REQUIREMENT_DETAIL_API_ENDPOINT+postData.id, decamelizedData).then( (successResponse) => {
             const responseData = successResponse.data;
             let device = camelizeKeys(responseData);
 
@@ -316,31 +316,20 @@ export function deleteInsuranceRequirementDetail(id, onSuccessCallback, onFailur
         // Generate our app's Axios instance.
         const customAxios = getCustomAxios();
 
-        const aURL = WORKERY_INSURANCE_REQUIREMENT_DETAIL_API_ENDPOINT+id+"/";
+        const aURL = WORKERY_INSURANCE_REQUIREMENT_DETAIL_API_ENDPOINT+id;
 
         customAxios.delete(aURL).then( (successResponse) => { // SUCCESS
-            const responseData = successResponse.data;
-            // console.log(successResult); // For debugging purposes.
-
-            let profile = camelizeKeys(responseData);
-
-            // Extra.
-            profile['isAPIRequestRunning'] = false;
-            profile['errors'] = {};
-
-            console.log("deleteInsuranceRequirementDetail | Success:", profile); // For debugging purposes.
-
             // Update the global state of the application to store our
             // user profile for the application.
             store.dispatch(
-                setInsuranceRequirementDetailSuccess(profile)
+                setInsuranceRequirementDetailSuccess(null)
             );
 
             // DEVELOPERS NOTE:
             // IF A CALLBACK FUNCTION WAS SET THEN WE WILL RETURN THE JSON
             // OBJECT WE GOT FROM THE API.
             if (onSuccessCallback) {
-                onSuccessCallback(profile);
+                onSuccessCallback(null);
             }
 
         }).catch( (exception) => { // ERROR
